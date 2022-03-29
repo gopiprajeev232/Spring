2af9a710c90b4ac8320c992b88482e3f5d988757
Gopi Prajeev <gopi.battula@zemosolabs.com>
2022-03-29 13:39:53 +0530

    Deleted some files

--- Event-Management-System-master/Event-Management-System-master/src/main/java/com/azeem/demo/repository/EventsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.azeem.demo.repository;
-
-import com.azeem.demo.entity.Events;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventsRepository extends JpaRepository<Events, Integer> {
-}
--- Event-Management-System-master/Event-Management-System-master/src/main/java/com/azeem/demo/repository/SpeakersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.azeem.demo.repository;
-
-import com.azeem.demo.entity.Speakers;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpeakersRepository extends JpaRepository<Speakers, Integer> {
-}
